6e75da559a79c8af83359ee4c19a4a2f2e9604b2
Rezwan Ul <rezwan360@live.com>
2017-11-21 05:00:30 +0200

    my first commit after build

---
+++ src/main/java/com/tartu/cmsspring/CmsSpringApplication.java
@@ -0,0 +1,12 @@
+package com.tartu.cmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tartu/cmsspring/CmsSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tartu.cmsspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmsSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
